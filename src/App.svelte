84f8efa5ef6d7b9ea4e6fa074b180965fcdd98e9
worthlesspandacollector <zarf5446@gmail.com>
2023-02-09 02:52:45 -0500

    first

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+ DRAGONMA BALLS ALL OVA UR FACE
+</main>
+
+<style>
+
+</style>
